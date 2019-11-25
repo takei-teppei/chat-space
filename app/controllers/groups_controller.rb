a6d3d1d157b6b2fef4fadcdb7ec4340a537f22e5
takei-teppei <boxmaikumanny348@gmail.com>
2019-11-25 21:35:18 +0900

    group_edit1

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,10 +21,19 @@ end
   end
 
   def update
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループを更新しました'
+    else
+      render :edit
+    end
   end
 
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [] )
+  end
+  
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
