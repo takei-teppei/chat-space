73b6da9b6e2caf11752d851cdaeb93ec6393e832
takei-teppei <boxmaikumanny348@gmail.com>
2019-11-30 15:39:29 +0900

    update1

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,55 @@
+$(function(){
+  function buildHTML(message){
+    // 「もしメッセージに画像が含まれていたら」という条件式
+    if (message.image.url) {
+      var html = `<div class="chat_contents__top--username">
+                    ${message.name}
+                  </div>
+                  <div class="chat_contents__top--datetimes">
+                    ${message.date}
+                  </div>
+                  <p class="chat_contents__message">
+                    ${message.content}
+                  </p>
+                  <img class="chat_contents__message" src="${message.image.url}">`
+    } 
+    else {
+      var html =`<div class="chat_contents__top--username">
+                  ${message.name}
+                 </div>
+                 <div class="chat_contents__top--datetimes">
+                  ${message.date}
+                 </div>
+                 <p class="chat_contents__message">
+                  ${message.content}
+                 </p>`
+    }
+    return html
+  }
+
+  $('#new_message').on("submit", function(e){
+    e.preventDefault()
+    var formData = new FormData(this);
+    var url = $(this).attr("action");
+    $.ajax({
+      url: url, 
+      type: 'POST',
+      data: formData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })  
+    .done(function(message){
+      var html = buildHTML(message);
+      $('.chat_contents').append(html)
+      $('#new_message')[0].reset();
+      $('.chat_contents').animate({ scrollTop: $('.chat_contents')[0].scrollHeight});
+    })
+    .fail(function(){
+      alert("メッセージ送信に失敗しました");
+    })
+    .always(function(){
+      $('.form__btn--submit').prop('disabled', false);
+    });
+  })
+});
